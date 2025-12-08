a417607794ea7eeea7c9df7ecbda50e0c0486306
enriquefabio785-sudo <enriquefabio785@gmail.com>
2025-12-09 00:19:21 +0100

    muchos cambios

--- funciones.py
+++ funciones.py
@@ -4,7 +4,8 @@ lista=['plaz_1.json', 'plaz_2.json', 'plaz_3.json', 'plaz_4.json', 'plaz_5.json'
  'plaz_6.json', 'plaz_7.json', 'plaz_8.json', 'plaz_9.json', 'plaz_10.json',
  'plaz_11.json', 'plaz_12.json', 'plaz_13.json', 'plaz_14.json', 'plaz_15.json',
  'plaz_16.json', 'plaz_17.json', 'plaz_18.json', 'plaz_19.json', 'plaz_20.json',
- 'plaz_21.json', 'plaz_22.json', 'plaz_23.json']
+ 'plaz_21.json', 'plaz_22.json', 'plaz_23.json',"plaz_24.json","plaz_25.json",
+ "plaz_26.json","plaz_27.json","plaz_28.json","plaz_29.json","plaz_30.json"]
 for i in lista:
      with open(f"E:/universidad/proyecto CD NO TOCAR/data/mypimes/{i}","r",encoding="utf-8") as f:
          datos=json.load(f)
@@ -13,11 +14,11 @@ for i in lista:
 def busqueda(tipo=None,marca=None,unidad=None,pesaje=None,precio=None):
     buscados=[]
     for x in lista_de_productos:
-        if tipo is not None and x["tipo"] != tipo:  
+        if tipo is not None and tipo not in x["tipo"]:  
           continue
-        if marca is not None and x["marca"] != marca:  
+        if marca is not None and  marca not in x["marca"]:  
           continue
-        if unidad is not None and x["unidad"] != unidad:  
+        if unidad is not None and  unidad not in x["unidad"]:  
           continue
         if pesaje is not None and x["pesaje"] != pesaje:  
           continue
@@ -43,6 +44,12 @@ def price_than_10gr(peso="none",precio="none"):
             return round((precio_num / peso_num) * 10, 1)
     except:
             return "los valores tienen que ser o deben poder ser llevados a int o float"
+
+def mediana(lista):
+  lista=sorted(lista)
+  if len(lista)%2!=0:
+      return lista[len(lista)//2]
+  return (lista[len(lista)//2]+lista[len(lista)//2-1])/2
 
 
     
